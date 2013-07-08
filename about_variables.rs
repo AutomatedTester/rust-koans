23f31f9d8262c5cd4ee58c89328228a8422958c5
AutomatedTester <dburns@mozilla.com>
2013-07-08 20:58:31 +0100

    added managed pointer test

--- about_variables.rs
+++ about_variables.rs
@@ -1,5 +1,12 @@
 #[test]
 fn should_show_variables_are_immutable_by_default(){
-    let foo = "I like foo";
+    let mut foo = "I like foo";
     foo = "I actually like baz";
+}
+
+#[test]
+fn should_show_a_managed_pointer(){
+    // A managed pointer allows us to work with mutable objects
+    let foo = [1, 2, 3, 4];
+    foo.push(5);
 }
