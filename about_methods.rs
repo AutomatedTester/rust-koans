82531051db01b2bd4d86f9be0199772c88a17372
AutomatedTester <dburns@mozilla.com>
2013-07-08 14:26:04 +0100

    adding methods koan

---
+++ about_methods.rs
@@ -0,0 +1,17 @@
+#[test]
+fn should_call_method_and_handle_response(){
+	// Return an integer
+	let foo = sum(4, 5);
+	assert!(foo == 9);
+}
+
+#[test]
+fn should_call_method_and_have_an_implicit_return(){
+	/*
+		Implicit returns have a little magic a thanks to the way
+		semicolons are handled
+	*/
+
+	let foo = sum2(4, 5);
+	assert!(foo == 9);
+}
